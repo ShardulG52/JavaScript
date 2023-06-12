6957eb5a7c36bde3369311a5e196023de0ef7cc6
Gautam-Mob <ShardulG@mobiquityinc.com>
2023-06-12 12:47:22 +0530

    task 9 completed

---
+++ Day 1/Task9.js
@@ -0,0 +1,23 @@
+function MergeObject(obj1 , obj2) {
+let ans = obj1;
+
+Object.keys(obj2).forEach((key) => {
+
+    ans[key]=obj1[key]?obj1[key]+obj2[key]: obj2[key];
+});
+return ans;
+
+}
+
+var obj1 = {
+    name : "Shardul Gautam",
+    empId : 71453,
+    state : "UP"
+};
+
+var obj2 = {
+    tech : "React",
+    state : "UP"
+};
+
+console.log(MergeObject(obj1,obj2));
